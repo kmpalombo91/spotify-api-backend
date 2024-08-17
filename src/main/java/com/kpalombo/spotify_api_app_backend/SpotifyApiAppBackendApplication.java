3fb47c78e834f95ececf60e1824e045129191f38
kmpalombo91 <kmpalombo91@gmail.com>
2024-08-16 21:25:06 -0500

    initialize project

---
+++ src/main/java/com/kpalombo/spotify_api_app_backend/SpotifyApiAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kpalombo.spotify_api_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyApiAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyApiAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kpalombo/spotify_api_app_backend/SpotifyApiAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kpalombo.spotify_api_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyApiAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
